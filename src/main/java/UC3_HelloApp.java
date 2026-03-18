c42b0f2404c075c107d06ea9ad15a6b414d64e15
sn0936-max <sn0936@srmist.edu.in>
2026-03-18 13:51:16 +0530

    Delete src/main/java/UC3_HelloApp.java

--- src/main/java/UC3_HelloApp.java
+++
@@ -1,9 +0,0 @@
-public class UC3_HelloApp {
-    public static void main(String[] args) {
-        String name="World";
-        if(args.length>0){
-            name=args[0];
-        }
-        System.out.println("Hello, "+name);
-    }
-}
